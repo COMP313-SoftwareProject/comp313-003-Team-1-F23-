ee3a0ecd899e55d2374a7a477707a201db2c9fdf
Sabeeh <muhammadsabeeh01@gmail.com>
2023-11-04 23:42:35 -0400

    2 unit tests for tutorial

--- Telum/Assets/Scripts/Player/PlayerMovement.cs
+++ Telum/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,14 +9,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpPower = 5f;
-    private float xInput;
-
-    [SerializeField] private Rigidbody2D rb;
+    public float xInput;
+    public Rigidbody2D rb;
 
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
-
-    [SerializeField] private Tilemap coins;
 
     #region Color Vars
     public PlayerColor currentColor;
@@ -37,9 +34,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
@@ -49,13 +46,25 @@ public class PlayerMovement : MonoBehaviour
         ColorSwitcher();
     }
 
-    #region Movement
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-        ColorSwitcher();
+        Move();
     }
 
+    #region Movement
+    public void Move()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+
+        if (xInput != 0)
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
+        }
+    }
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
